f2d15dc49788e628dece1ffc60471811430a0164
Fatih Karatay <m61353@tdom.local>
2020-02-26 16:38:31 -0500

    Triggering event callbacks

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,9 +117,76 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log('hi there');
-},{}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+    this.events = {};
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length == 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      callback();
+    });
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./models/User");
+
+var user = new User_1.User({
+  name: 'fatih',
+  age: 25
+});
+user.on('change', function () {
+  console.log('change #1');
+});
+user.on('change', function () {
+  console.log('change #2');
+});
+user.on('click', function () {
+  console.log('click was clicked');
+});
+user.trigger('change');
+user.trigger('click');
+},{"./models/User":"src/models/User.ts"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +214,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61539" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51117" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hi there');
+import { User } from './models/User';
+
+const user = new User({name: 'fatih', age: 25});
+
+user.on('change', () => {
+  console.log('change #1');
+});
+user.on('change', () => {
+  console.log('change #2');
+});
+user.on('click', () => {
+  console.log('click was clicked');
+});
+
+user.trigger('change');
+user.trigger('click');
